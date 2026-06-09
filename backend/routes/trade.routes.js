a8203d6842fa5491756a9e69e8ea207b2f682385
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-06-09 13:15:16 +0300

    Update trade.routes.js

--- backend/routes/trade.routes.js
+++ backend/routes/trade.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { DerivEngine } from "../../services/deriv-engine/deriv.js";
+import { DerivEngine } from "../services/deriv-engine/deriv.js";
 
 const router = express.Router();
 
