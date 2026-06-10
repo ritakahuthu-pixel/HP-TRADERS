fd3e06a4b7a7a9c46edb639f6ba3d043a087c383
ritakahuthu-pixel <ritakahuthu@gmail.com>
2026-06-10 12:08:57 +0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -13,7 +13,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const API_URL = 'https://hp-traders-xxxx.onrender.com';
 
 app.listen(PORT, () => {
   console.log(`Frontend running on port ${PORT}`);
